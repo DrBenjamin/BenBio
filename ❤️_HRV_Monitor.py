a0e43b874629d8a70139abcb94857491ebeb9cbb
Benjamin Gross <ben@benbox.org>
2024-09-03 09:58:00 +0200

    minor improvements

--- ❤️_HRV_Monitor.py
+++ ❤️_HRV_Monitor.py
@@ -4,11 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def csv_reader(file_path):
-    # Lesen der CSV-Datei
     df = pd.read_csv(file_path)
-    # Extrahieren der SDNN-Werte aus der Spalte 'SDNN'
-    sdnn_values = df['SDNN'].tolist()
-    return sdnn_values
+    return df['SDNN'].tolist()
 
 # Load SDNN values
 csv_file = st.file_uploader('Upload CSV file with SDNN values', type='csv')
@@ -16,15 +13,15 @@ if csv_file:
   sdnn_values = csv_reader(csv_file)
   print(sdnn_values)
 
-  # Einstufung des Stresslevels anhand der RMSSD-Werte
+  # Categorizing Stresslevels
   if np.mean(sdnn_values) > 50:
-      stress_level = "Niedriger Stress"
+      stress_level = "Low stress"
   elif 25 < np.mean(sdnn_values) <= 50:
-      stress_level = "Mittlerer Stress"
+      stress_level = "Medium stress"
   else:
-      stress_level = "Hoher Stress"
+      stress_level = "High stress"
 
-  # Visualisierung der HRV
+  # Visualising HRV
   st.title ('HRV Analyse: SDNN')
   fig, ax = plt.subplots(figsize=(10, 5), facecolor='none')
   ax.set_facecolor('none')
@@ -32,8 +29,8 @@ if csv_file:
   ax.set_title('Standard Deviation of the inter-beat (RR) intervals between normal heartbeats (SDNN)', color='gray')
   ax.set_xlabel('SDNNs', color='gray')
   ax.set_ylabel('Intervalls in ms', color='gray')
-  ax.axhline(y=np.mean(sdnn_values), color='r', linestyle='--', label=f"Durchschnitt ({np.mean(sdnn_values):.2f} ms)")
-  ax.text(0, np.mean(sdnn_values), 'Wertung: {:.2f} ms\nStresslevel: {}'.format(np.mean(sdnn_values), stress_level), fontsize=10, bbox=dict(facecolor='white', alpha=0.5))
+  ax.axhline(y=np.mean(sdnn_values), color='r', linestyle='--', label=f"Average ({np.mean(sdnn_values):.2f} ms)")
+  ax.text(0, np.mean(sdnn_values), 'Avg. value: {:.2f} ms\nStresslevel: {}'.format(np.mean(sdnn_values), stress_level), fontsize=10, bbox=dict(facecolor='white', alpha=0.5))
   ax.legend()
   ax.grid()
   st.pyplot(fig)
